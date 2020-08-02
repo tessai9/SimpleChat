bc51a18e4ddd52acc22d8871eec9e8d00a382666
tesao <shimeji9@gmail.com>
2020-08-02 16:08:44 +0900

    add creating client function

--- src/p2p_node.rs
+++ src/p2p_node.rs
@@ -1,5 +1,10 @@
-// connection module for P2P
-use libp2p::{Multiaddr, Transport, tcp::TcpConfig, identity::Keypair};
+// P2P Node Behavior
+use libp2p::{
+    Multiaddr,
+    Transport,
+    tcp::TcpConfig,
+    identity::ed25519::Keypair
+};
 
 
 #[derive(Debug, Clone)]
@@ -13,4 +18,18 @@ pub fn make_connection(target_addr: &str) {
 
     let tcp = TcpConfig::new();
     tcp.dial(addr);
+}
+
+pub fn create_client(target_addr: &str) {
+    let local_key = Keypair::generate();
+    let public_key = local_key.public();
+    let _transport = libp2p::build_development_transport(local_key).expect("failed to setup transport");
+
+    let mut addr = format!("{}{}{}", "/ip4/", target_addr.to_string(), "/tcp/20500");
+    let addr: Multiaddr = addr.parse().expect("invalid multiaddr");
+
+    _transport.dial(addr);
+    _transport.listen_on(addr);
+
+    // create floodsub or gossipsub
 }
